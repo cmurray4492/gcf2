ca344b0f6ad335671a0899bc6b8c864baf5c5e39
cmurray4492 <cmurray4492@gmail.com>
2020-12-13 20:10:10 -0600

    ads.txt 2

--- gcf/urls.py
+++ gcf/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,11 +10,11 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('tinymce/', include('tinymce.urls')),
     path('blog/', include('gcfblog.urls')),
+    path('ads.txt', RedirectView.as_view(url=staticfiles_storage.url("ads.txt"))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 """
 urlpatterns = [
-    path('tinymce/', include('tinymce.urls')),
     path('chruches/', include('chruches.urls')),
     path('prayers/', include('prayers.urls')),
 """
