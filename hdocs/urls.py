24a18dd02d98b52f8bccf96063a9faa9e370136c
cmurray4492 <cmurray4492@gmail.com>
2021-01-11 21:02:14 -0600

    Build HDocs 1

--- hdocs/urls.py
+++ hdocs/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.hdocs, name="hdocs"),
     path('declaration_of_independence', views.declaration_of_independence, name="declaration_of_independence"),
+    path('us_constitution', views.us_constitution, name="us_constitution"),
+    path('us_bill_of_rights', views.us_bill_of_rights, name="us_bill_of_rights"),
 ]
-
-#    path('about', views.about, name="about"),
-#    path('contactus', views.contactus, name="contactus"),
-#   path('usefulinfo', views.usefulinfo, name="usefulinfo"),
--- hdocs/views.py
+++ hdocs/views.py
@@ -7,3 +7,11 @@ def hdocs(request):
 
 def declaration_of_independence(request):
     return render(request, 'hdocs/declaration_of_independence.html')
+
+
+def us_constitution(request):
+    return render(request, 'hdocs/us_constitution.html')
+
+
+def us_bill_of_right(request):
+    return render(request, 'hdocs/us_bill_of_rights')
